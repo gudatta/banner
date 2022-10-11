50fdf4179995e701cbc1bfbe45499da9fcb62220
Gunjan Datta <gudatta@microsoft.com>
2022-10-11 11:35:36 -0400

    Initial code check-in.

---
+++ src/extensions/banner/loc/en-us.js
@@ -0,0 +1,7 @@
+define([], function() {
+  return {
+    "CACHE_KEY": "BannerApplicationCustomizer",
+    "LOG_KEY": "BannerApplicationCustomizer",
+    "Title": "BannerApplicationCustomizer"
+  }
+});
---
+++ src/extensions/banner/loc/myStrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IBannerApplicationCustomizerStrings {
+  CACHE_KEY: string;
+  LOG_KEY: string;
+  Title: string;
+}
+
+declare module 'BannerApplicationCustomizerStrings' {
+  const strings: IBannerApplicationCustomizerStrings;
+  export = strings;
+}
